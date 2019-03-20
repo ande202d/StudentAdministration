727fcb6e1f46f3c199c6e6e10d4e3ac1da615848
BuildTools <unconfigured@null.spigotmc.org>
2019-03-20 13:06:53 +0100

    added s3 from home

--- StudentAdministration/Program.cs
+++ StudentAdministration/Program.cs
@@ -12,8 +12,10 @@ namespace StudentAdministration
         {
             Student s1 = new Student("Anders", 1);
             Student s2 = new Student("Jacob", 2);
+            Student s3 = new Student("Mikkel", 3);
             Console.WriteLine(s1);
             Console.WriteLine(s2);
+            Console.WriteLine(s3);
 
             Console.ReadKey();
         }
